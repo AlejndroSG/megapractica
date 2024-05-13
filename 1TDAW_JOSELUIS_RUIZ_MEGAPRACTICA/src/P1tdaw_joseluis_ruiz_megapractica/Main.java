321d07af41edc9171c38a50ae0e085dc2e543312
AlejndroSG <alex2005sg@gmail.com>
2024-05-13 12:50:38 +0200

    13/05/2024

--- 1TDAW_JOSELUIS_RUIZ_MEGAPRACTICA/src/P1tdaw_joseluis_ruiz_megapractica/Main.java
+++ 1TDAW_JOSELUIS_RUIZ_MEGAPRACTICA/src/P1tdaw_joseluis_ruiz_megapractica/Main.java
@@ -1,6 +1,6 @@
 package P1tdaw_joseluis_ruiz_megapractica;
 
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Scanner;
 
 
@@ -10,20 +10,58 @@ public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Scanner sc = new Scanner (System.in);
         Conexion c = new Conexion();
+        String consulta1 = "";
         System.out.println("Bienvenido a recetasPAL");
-        System.out.println("¿Estás registrado?");
+        System.out.println("¿Estás registrado?(S/N)");
         char opc;
         do{
             opc = sc.next().toUpperCase().charAt(0);
         }while(opc != 'S' && opc != 'N');
         
         if(opc == 'S'){
+            sc.nextLine();
             System.out.println("Introduce nombre de usuario");
-            String nom_usu = sc.nextLine();
+            String nom_usu = sc.nextLine().toLowerCase();
             System.out.println("Introduce la contraseña");
             String contr = sc.nextLine();
+            consulta1 = "select nameusu, password from usuarioR where password = '" + contr + "' and nameusu = '" + nom_usu + "'";
             
-            ResulSet rs = c.select("nameusu, password from usuarioR where password ="+contr+" and nameusu ="+nom_usu);
+            c.select(consulta1);
+        }else{
+            System.out.println("¿Quiéres registrarte?(S/N)");
+            do{
+                opc = sc.next().toUpperCase().charAt(0);
+            }while(opc != 'S' && opc != 'N');
+            if(opc == 'S'){
+                sc.nextLine();
+                System.out.println("Introduce nombre de usuario");
+                String nom_usu = sc.nextLine().toLowerCase();
+                System.out.println("Introduce la contraseña");
+                String contr = sc.nextLine();
+                
+                consulta1 = "select count(*) from usuarioR where password = '" + contr + "' and nameusu = '" + nom_usu + "';";
+                Statement st = c.getCon().createStatement();
+                ResultSet rs = st.executeQuery(consulta1);
+                rs.next();
+                int count = rs.getInt(1);
+                System.out.println(count);
+                
+                
+                
+                if(count < 1){
+                    System.out.println("Tu cuenta no existe , vamos a crear una!!");
+                    System.out.println("Introduce el nombre");
+                    String nom = sc.nextLine();
+                    System.out.println("Introduce el apellido");
+                    String apell = sc.nextLine();
+                    System.out.println("Introduce el correo electrónico");
+                    String corr = sc.nextLine();
+                    String consulta = "insert into usuarioR values ('"+ nom + "', '"+ apell + "', '"+ corr +"', '"+ nom_usu +"', 'usuarioR', '"+ contr +"'";
+                    c.insert(consulta);
+                }
+                
+                c.cierre();
+            }
         }
     }   
     
